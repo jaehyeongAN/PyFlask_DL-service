42abe731095caf60056009dbaa793a49d64f2345
jaehyeongAN <nonamed000000@gmail.com>
2019-11-16 11:34:34 +0900

    first nst

---
+++ flask_deep/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask, escape, request,  Response, g, make_response
+from flask.templating import render_template
+from werkzeug import secure_filename
+import os
+from . import neural_style_transfer
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+	a = 1
+	b = 2
+	sum_ab = a+b
+	return render_template('index.html', sum_ab=sum_ab)
+
+
+@app.route('/info')
+def info():
+	return render_template('info.html')
+
+
+@app.route('/get_image')
+def test():
+	return render_template('get_image.html')
+
+@app.route('/object_detection')
+def object_detection():
+	return render_template('object_detection.html')
+
+
+@app.route('/post_image', methods=['GET','POST'])
+def post():
+	if request.method == 'POST':
+		# Reference Image
+		refer_img = request.form['refer_img']
+		refer_img_path = 'images/'+str(refer_img)
+
+		# User Image (target image)
+		user_img = request.files['user_img']
+		user_img.save('./flask_deep/static/images/'+str(user_img.filename))
+		user_img_path = 'images/'+str(user_img.filename)
+
+		# Neural Style Transfer 
+		transfer_img = neural_style_transfer.main(refer_img_path, user_img_path)
+		transfer_img_path = 'images/'+str(transfer_img.split('/')[-1])
+
+
+
+	
+	return render_template('post_image.html', 
+					refer_img=refer_img_path, user_img=user_img_path, transfer_img=transfer_img_path)
