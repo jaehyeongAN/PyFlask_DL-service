ce736fe6906eccd9d7136f416ef70d4286fcad78
jaeheyong1 <jaehyeong@pioncorp.com>
2021-04-05 23:53:57 +0900

    update code

--- flask_deep/__init__.py
+++ flask_deep/__init__.py
@@ -1,11 +1,11 @@
 import os, sys
 real_path = os.path.dirname(os.path.realpath(__file__))
-sub_path = "/".join(real_path.split("/")[:-1])
+sub_path = os.path.split(real_path)[0]
 os.chdir(sub_path)
 
 from flask import Flask, escape, request,  Response, g, make_response
 from flask.templating import render_template
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from . import neural_style_transfer
 from .darkflow_yolo import processing_images
 
